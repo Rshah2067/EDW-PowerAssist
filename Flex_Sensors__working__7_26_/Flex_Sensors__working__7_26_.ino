e4493fe4baf50a36f5caf41bcad8605bc77f3118
Maya <mayalobel@gmail.com>
2019-07-29 12:02:23 -0400

    flex sensors changed

--- Flex_Sensors__working__7_26_/Flex_Sensors__working__7_26_.ino
+++ Flex_Sensors__working__7_26_/Flex_Sensors__working__7_26_.ino
@@ -1,11 +1,13 @@
 
 #include "FlexLibrary.h"
 int deg;
+int deg3;
 int flex_Min = 51; 
 int flex_Max = 126;
 Flex flex(A0, flex_Min, flex_Max, 10, RUN_AVG, 0);
 
 int deg2;
+int deg4;
 int flex_Min2 = 4; 
 int flex_Max2 = 30;
 Flex flex2(A1, flex_Min2, flex_Max2, 10, RUN_AVG, 0);
@@ -34,6 +36,8 @@ void loop() {
   debug2 = ((String)"Flex 2: " + flex2.getSensorValue());
   deg = map(deg, flex_Min, flex_Max, 0, 90);
   deg2 = map(deg2, flex_Min2, flex_Max2, 0, 90);
+  delay(200);
+
   Serial.println(debug);
   Serial.println(debug2);
   Serial.print("Flex 1 Degrees: ");
@@ -41,6 +45,30 @@ void loop() {
   Serial.print("Flex 2 Degrees: ");
   Serial.println(deg2);
   Serial.println("");
+
+  flex.updateVal();
+  flex2.updateVal();
+  deg3 = flex.getSensorValue();
+  deg4 = flex2.getSensorValue();
+  debug = ((String)"Flex 1: " + flex.getSensorValue());
+  debug2 = ((String)"Flex 2: " + flex2.getSensorValue());
+  deg3 = map(deg3, flex_Min, flex_Max, 0, 90);
+  deg4 = map(deg4, flex_Min2, flex_Max2, 0, 90);
+  
+  Serial.println(debug);
+  Serial.println(debug2);
+  Serial.print("Flex 1 Degrees: ");
+  Serial.println(deg3);
+  Serial.print("Flex 2 Degrees: ");
+  Serial.println(deg4);
+
+  int difference1 = deg - deg3;
+  int difference2 = deg - deg4;
+
+  Serial.print(difference1);
+  Serial.println(difference2);
+  Serial.println("");
+  
   delay(200);
 
 
